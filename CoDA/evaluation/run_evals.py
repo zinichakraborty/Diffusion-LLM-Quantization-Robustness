d9bca16299eee2eaf967b100a383711aaa480e17
Gururaj Mahendra Deshpande <gdeshpande9@login-ice-1.pace.gatech.edu>
2025-10-18 01:54:01 -0400

    add helpful eval scripts

---
+++ CoDA/evaluation/run_evals.py
@@ -0,0 +1,59 @@
+import argparse
+from pathlib import Path
+from utils import EVALUATION_SETS, SCRIPT_TEMPLATE, JOB_NAME_TEMPLATE, LOG_FILE_PATH_TEMPLATE, MEM, GRES_GPU, TIME
+import pyslurm
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--results-dir", type=str, default="results")
+    return parser.parse_args()
+
+def create_results_dir(results_dir):
+    results_dir = Path(results_dir)
+    results_dir.mkdir(parents=True, exist_ok=True)
+    return results_dir
+
+def check_model(model):
+    if not Path(model).exists() and not model.startswith("Salesforce/"):
+        raise FileNotFoundError(f"Model path {model} does not exist or is not a Salesforce model")
+
+def submit_job(script, job_name, log_output):
+    desc = pyslurm.JobSubmitDescription(
+        name=job_name,
+        time_limit=TIME,
+        nodes=1,
+        memory_per_node=MEM,
+        gres_per_node=GRES_GPU,
+        standard_output=log_output,
+        standard_error=log_output,
+        script=script
+    )
+    job_id = desc.submit()
+    return job_id
+
+def create_jobs(model, results_dir):
+    job_ids = []
+    for evaluation_set in EVALUATION_SETS:
+        script = SCRIPT_TEMPLATE.format(model=model, results_dir=results_dir, evaluation_set=evaluation_set).strip()
+        job_name = JOB_NAME_TEMPLATE.format(evaluation_set=evaluation_set, model=model).strip()
+        log_output = LOG_FILE_PATH_TEMPLATE.format(evaluation_set=evaluation_set, model=model.replace("/", "_")).strip()
+        job_id = submit_job(script, job_name, log_output)
+        job_ids.append(job_id)
+        print(f"Submitted job {job_id} for {evaluation_set} evaluation on {model}")
+
+def main():
+    args = parse_args()
+    model = args.model
+    results_dir = args.results_dir
+    
+    check_model(model)
+    results_dir = create_results_dir(results_dir)
+    print(f"Results will be stored in {results_dir}")
+    
+    create_jobs(model, results_dir)
+    
+    print("Jobs submitted successfully")
+
+if __name__ == "__main__":
+    main()
---
+++ CoDA/evaluation/utils.py
@@ -0,0 +1,33 @@
+JOB_NAME_TEMPLATE = "Task_{evaluation_set}_evaluation_on_{model}"
+NODES = 1
+MEM = "500G"
+GRES_GPU = "gpu:l40s:4"
+TIME = "5:00:00"
+LOG_FILE_PATH_TEMPLATE = "./logs/{evaluation_set}_evaluation_on_{model}.out"
+MAIL_TYPE = "BEGIN,END,FAIL"
+
+SCRIPT_TEMPLATE = """
+#!/bin/bash
+
+cd $SLURM_SUBMIT_DIRs
+
+module load uv
+
+export HF_ALLOW_CODE_EVAL=1
+export SLURM_INCLUDE_DIR=/opt/slurm/current/include
+export SLURM_LIB_DIR=/opt/slurm/current/lib
+
+srun uv run accelerate launch -m lm_eval \
+    --model diffllm \
+    --model_args pretrained={model},trust_remote_code=True,max_new_tokens=768,diffusion_steps=768,dtype="bfloat16",temperature=0.1,top_p=0.9,alg="entropy" \
+    --tasks humaneval_instruct \
+    --device cuda \
+    --batch_size 4 \
+    --num_fewshot 0 \
+    --output_path {results_dir}/{evaluation_set}_evaluation_on_{model}.json
+    --log_samples \
+    --confirm_run_unsafe_code \
+    --apply_chat_template true
+"""
+
+EVALUATION_SETS = ["humaneval_instruct", "humaneval_plus", "mbpp_instruct", "mbpp_plus"]
