dadd098e4a26a9728d2f84a15f3d1498278ca09f
Gururaj Mahendra Deshpande <gdeshpande9@login-ice-1.pace.gatech.edu>
2025-10-19 15:11:44 -0400

    finish gptq

--- CoDA/evaluation/run_evals.py
+++ CoDA/evaluation/run_evals.py
@@ -33,12 +33,15 @@ def submit_job(script, job_name, log_output):
     job_id = desc.submit()
     return job_id
 
+def safe_model_name(model):
+    return model.replace("/", "__")
+
 def create_jobs(model, results_dir):
     job_ids = []
     for evaluation_set in EVALUATION_SETS:
-        script = SCRIPT_TEMPLATE.format(model=model, results_dir=results_dir, evaluation_set=evaluation_set, batch_size=BATCH_SIZE).strip()
-        job_name = JOB_NAME_TEMPLATE.format(evaluation_set=evaluation_set, model=model.replace("/", "_")).strip()
-        log_output = LOG_FILE_PATH_TEMPLATE.format(evaluation_set=evaluation_set, model=model.replace("/", "_")).strip()
+        script = SCRIPT_TEMPLATE.format(model=model, safe_model_name=safe_model_name(model), results_dir=results_dir, evaluation_set=evaluation_set, batch_size=BATCH_SIZE).strip()
+        job_name = JOB_NAME_TEMPLATE.format(evaluation_set=evaluation_set, model=safe_model_name(model)).strip()
+        log_output = LOG_FILE_PATH_TEMPLATE.format(evaluation_set=evaluation_set, model=safe_model_name(model)).strip()
         job_id = submit_job(script, job_name, log_output)
         job_ids.append(job_id)
         print(f"Submitted job {job_id} for {evaluation_set} evaluation on {model}")
